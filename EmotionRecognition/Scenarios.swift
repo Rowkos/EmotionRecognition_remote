2e5fcf9512f01c6e8ff9100b1bdcac9308e76749
Rowkos <rowankoskoff@gmail.com>
2023-09-09 18:05:07 -0700

    final tweaks to scenario presentation

--- EmotionRecognition/Scenarios.swift
+++ EmotionRecognition/Scenarios.swift
@@ -19,12 +19,12 @@ struct Scenarios
             let adjustedIndex = Int(Double(rows.count) * (Double(index) / 100.0))
             rows.removeFirst()
             var subString = rows[index]
-            let range = subString.index(subString.startIndex, offsetBy: 3)..<subString.index(subString.endIndex, offsetBy: -2)
+            let range = subString.index(subString.startIndex, offsetBy: 2)..<subString.index(subString.endIndex, offsetBy: -2)
             subString = String(subString[range])
             let splitString = subString.components(separatedBy: "\",")[0]
             let finalRange = splitString.index(splitString.startIndex, offsetBy: 1)..<splitString.index(splitString.endIndex, offsetBy: -1)
             let finalSplitString = " " + splitString[finalRange] + "."
-            returnString = String(finalSplitString).replacingOccurrences(of: "..", with: ".").replacingOccurrences(of: "  ", with: " ").replacingOccurrences(of: "\"\"", with: "\"")
+            returnString = String(finalSplitString).replacingOccurrences(of: "..", with: ".").replacingOccurrences(of: "  ", with: " ").replacingOccurrences(of: "\"\"", with: "\"").replacingOccurrences(of: " \"", with: " ")
             
         } catch {
             print(error)
@@ -44,8 +44,9 @@ struct Scenarios
             var subString = rows[index]
             let range = subString.index(subString.startIndex, offsetBy: 3)..<subString.index(subString.endIndex, offsetBy: -2)
             subString = String(subString[range])
-            var splitString = " " + String(subString.components(separatedBy: "\",\"")[1]) + "."
-            returnString = splitString.replacingOccurrences(of: ". .", with: ".").replacingOccurrences(of: "  ", with: " ").replacingOccurrences(of: "..", with: ".").replacingOccurrences(of: "\"\"", with: "\"")
+            var splitString = " " + String(subString.components(separatedBy: "\",")[1]) + "."
+            returnString = splitString.replacingOccurrences(of: ". .", with: ".").replacingOccurrences(of: "  ", with: " ").replacingOccurrences(of: "..", with: ".").replacingOccurrences(of: "\"\"", with: "\"").replacingOccurrences(of: "\" ", with: " ")
+            
             
         } catch {
             print(error)
